d28f89866d6390cfc64e2a2fd32fac788b37098b
pianohagens <pianohagens@gmail.com>
2018-01-17 14:32:08 -0800

    ITC 162_Assignment 2_01-17-2018_tested Starting from scratch, create your own UI layout for an android app. The layout should contain a textView, an editView and a button widget. Use the Infer constraint button (shown below circled with a red pen) to automatically create your constraints.
    Add resource strings for your text and widgets.
    The TextView should display your name
    The EditView should display “Enter favorite color”
    The button should display Submit
    Run your app in the emulator to make sure it works.
    Your assignment will be graded in class after due date.
    Note: We will soon learn how to use Android studio github integration, after which you will start adding your code  to your repository.

--- app/src/main/java/com/example/piano/myhomeapplication/MainActivity.java
+++ app/src/main/java/com/example/piano/myhomeapplication/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.piano.myhomeapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView displyresult;
+    EditText inputTxt;
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputTxt = (EditText)findViewById(R.id.inputTxt);
+        displyresult = (TextView)findViewById(R.id.displyresult);
+        submitButton = (Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                displyresult.setText(inputTxt.getText().toString());
+            }
+        });
+
+
     }
 }
